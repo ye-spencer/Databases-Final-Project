70c294c04db814406300286cccf2ff78ea90e9d2
Spencer Ye <spencerye425@gmail.com>
2025-12-01 16:49:05 -0500

    Create boilerplate next.js project

---
+++ predict-the-centenni-podium/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Predict the Centenni Podium",
+  description: "Predicting the Centenni Podium by Mirra Klimov and Spencer Ye",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ predict-the-centenni-podium/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Predict the Centenni Podium</h1>
+    </div>
+  );
+}
